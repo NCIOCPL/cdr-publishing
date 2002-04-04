b72802e5d2df0544f729a4e9d4bf60d3fe464f1c
Bob Kline <bkline@rksystems.com>
2002-04-04 22:44:55 +0000

    <R1604> Modified to match new cdrpub.py.

--- cdr/Publishing/Publish.py
+++ cdr/Publishing/Publish.py
@@ -2,8 +2,11 @@
 #
 # Bridge PublishingService.py to cdrpub.py
 #
-# $Id: Publish.py,v 1.1 2002-02-20 22:25:10 Pzhang Exp $
+# $Id: Publish.py,v 1.2 2002-04-04 22:44:55 bkline Exp $
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2002/02/20 22:25:10  Pzhang
+# First version of GOOD publish.py.
+#
 #
 #----------------------------------------------------------------------
 
@@ -11,9 +14,10 @@ import sys, string, cdrpub, time
 
 log = "d:/cdr/log/publish.log" 
 jobId = string.atoi(sys.argv[1])
-open(log, "a").write("Job %d: Started at: %s\n" % \
-    (jobId, time.ctime(time.time())))
-cdrpub.Publish("", "", "", [], [], jobId = jobId).publish()
-open(log, "a").write("Job %d: Ended at: %s\n" % \
-    (jobId, time.ctime(time.time())))
+divider = "=" * 60
+open(log, "a").write("Job %d: %s\nJob %d: Started at: %s\n" % \
+    (jobId, divider, jobId, time.ctime(time.time())))
+cdrpub.Publish(jobId).publish()
+open(log, "a").write("Job %d: Ended at: %s\nJob %d: %s\n" % \
+    (jobId, time.ctime(time.time()), jobId, divider))
        
