71f773c5d21905cea26dd9e490e4e6bc6d7939b1
ameyer <webcommauto@users.noreply.github.com>
2002-08-02 03:45:29 +0000

    <R2239> Added batch job initiation and logging.

--- cdr/Publishing/PublishingService.py
+++ cdr/Publishing/PublishingService.py
@@ -1,8 +1,11 @@
 #
 # This script starts the publishing service.
 #
-# $Id: PublishingService.py,v 1.5 2002-02-20 22:25:10 Pzhang Exp $
+# $Id: PublishingService.py,v 1.6 2002-08-02 03:45:29 ameyer Exp $
 # $Log: not supported by cvs2svn $
+# Revision 1.5  2002/02/20 22:25:10  Pzhang
+# First version of GOOD publish.py.
+#
 # Revision 1.4  2002/02/20 15:10:14  pzhang
 # Modified SCRIPT to point to /cdr/lib/python/publish.py. Avoided duplicate
 # files in /cdr/publishing/publish.py this way.
@@ -16,10 +19,16 @@
 #
 #
 
-import cdrdb, os, time, cdr, sys, string
+import cdrdb, cdrbatch, os, time, cdr, sys, string
 
+# Sleep time between checks for things to do
 sleepSecs = len(sys.argv) > 1 and string.atoi(sys.argv[1]) or 10
-SCRIPT = cdr.BASEDIR + "/publishing/publish.py"
+
+# Script and log file for publishing
+PUBSCRIPT = cdr.BASEDIR + "/publishing/publish.py"
+PUBLOG    = cdr.DEFAULT_LOGDIR + "/publish.log"
+
+# Publishing queries
 query  = """\
 SELECT id
   FROM pub_proc
@@ -30,12 +39,23 @@ UPDATE pub_proc
    SET status = 'Started'
  WHERE status = 'Ready'
 """
+
+# Query for batch job initiation
+batchQry = "SELECT id, command FROM batch_job WHERE status='%s'" %\
+           cdrbatch.ST_QUEUED
+
+# Logfile for publishing
+PUBLOG = cdr.DEFAULT_LOGDIR + "/publish.log"
+
 conn = None
 while 1:
     try:
+        # Connection for all efforts
         if not conn:
             conn = cdrdb.connect("CdrPublishing")
         cursor = conn.cursor()
+
+        # Publishing
         cursor.execute(query)
         rows = cursor.fetchall()
         if len(rows):
@@ -45,10 +65,74 @@ while 1:
         cursor = None
         for row in rows:
             #print "publishing job %d" % row[0]
-            os.spawnv(os.P_NOWAIT, cdr.PYTHON, ("CdrPublish", SCRIPT,
+            os.spawnv(os.P_NOWAIT, cdr.PYTHON, ("CdrPublish", PUBSCRIPT,
                                                 str(row[0])))
+
+        # Batch jobs
+        try:
+            cursor = conn.cursor()
+            cursor.execute(batchQry)
+            rows = cursor.fetchall()
+
+            if len (rows):
+                # Process each queued job
+                for jobId, cmd in rows:
+                    # Is the command cdr relative or absolute?
+                    if not os.path.isabs (cmd):
+                        cmd = cdr.BASEDIR + '/' + cmd
+
+                    # By convention, all jobs take a job id as last parameter
+                    script = "%s %d" % (cmd, jobId)
+
+                    # Distinguish scripts requiring an interpreter from others
+                    # Python script
+                    if cmd.endswith ('.py'):
+                        cmd = cdr.PYTHON
+
+                    # Perl script (may never be used)
+                    elif cmd.endswith ('.pl'):
+                        cmd = cdr.PERL
+
+                    # Executable or .bat/.cmd file
+                    else:
+                        cmd = script
+                        script = ""
+
+                    # Indicate that we are initiating job
+                    cdr.logwrite ("Daemon: about to show initiation of %s" %\
+                                  script, cdrbatch.LF)
+                    cdrbatch.sendSignal (conn, jobId, cdrbatch.ST_INITIATING,
+                                         cdrbatch.PROC_DAEMON)
+                    cdr.logwrite ("Daemon: back from initiation", cdrbatch.LF)
+
+                    # Done with cursor
+                    conn.commit()
+                    cursor.close()
+
+                    # Spawn the job
+                    # Haven't found a good way to find out if it worked
+                    # Return code doesn't help much
+                    # Called job should update status to cdrbatch.ST_INPROCESS
+                    cdr.logwrite ("Daemon: about to spawn: %s %s" % \
+                                  (cmd, script), cdrbatch.LF)
+                    os.spawnv (os.P_NOWAIT, cmd, (cmd, script))
+
+                    # Record it
+                    cdr.logwrite ("Daemon: spawned: %s %s" % (cmd, script),
+                                  cdrbatch.LF)
+        # Log any errors
+        except cdrbatch.BatchException, be:
+            cdr.logwrite ("Daemon - batch execution error: %s", str(be),
+                          cdrbatch.LF)
+        except cdrdb.Error, info:
+            cdr.logwrite ("Daemon - batch database error: %s" % info[1][0],
+                          cdrbatch.LF)
+            conn = None
+
     except cdrdb.Error, info:
-        # XXX Change this to log errors (add logging to cdr.py)
-        print 'Database failure: %s' % info[1][0]
+        # Log publishing job initiation errors
+        cdr.logwrite ('Database failure: %s' % info[1][0], PUBLOG)
         conn = None
+
+    # Do it all again after a decent interval
     time.sleep(sleepSecs)
