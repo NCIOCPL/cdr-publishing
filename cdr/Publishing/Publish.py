638f98e1ec2f42c731d59cea1dda51ad1738a172
Bob Kline <bkline@rksystems.com>
2004-06-01 20:31:56 +0000

    <R5123> Caught SystemExit, thrown by the mailers.

--- cdr/Publishing/Publish.py
+++ cdr/Publishing/Publish.py
@@ -2,8 +2,11 @@
 #
 # Bridge PublishingService.py to cdrpub.py
 #
-# $Id: Publish.py,v 1.4 2002-07-26 19:59:44 pzhang Exp $
+# $Id: Publish.py,v 1.5 2004-06-01 20:31:56 bkline Exp $
 # $Log: not supported by cvs2svn $
+# Revision 1.4  2002/07/26 19:59:44  pzhang
+# Imported cdrdb.
+#
 # Revision 1.3  2002/04/18 01:18:05  bkline
 # Added exception handling code.
 #
@@ -30,5 +33,8 @@ except cdrdb.Error, info:
     open(log, "a").write("Job %d: Database failure: %s\n" % (jobId, info[1][0]))
 except StandardError, arg:
     open(log, "a").write("Job %d: Failure: %s\n" % (jobId, arg[0]))
+except SystemExit:
+    # The mailers invoke sys.exit(0) when they're done, raising this exception.
+    pass
 except:
     open(log, "a").write("Job %d: Unexpected failure\n" % jobId)
