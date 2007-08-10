4bce7c126863a8157921038dbf91a5f971151424
Volker Englisch <venglisch@users.noreply.github.com>
2007-08-10 16:50:15 +0000

    <R7613> Finalized copy of FTP script to copy vendor/NLM data.

--- cdr/Publishing/FtpCTGovData.py
+++ cdr/Publishing/FtpCTGovData.py
@@ -2,35 +2,31 @@
 #
 # File Name: $RCSFile:$
 #            ==============
-# Package and submit interim update documents (mid-month updates) to the 
-# CIPSFTP server and store files in the directory cdr.
-# The documents are located in subdirectories named JobNNN with NNN
-# being an integer number.  Provide all jobIDs for the documents that 
-# should be packaged within one subdirectory on the FTP server as a
-# command line argument.
-# For instance, if an interim update ran under JobID=1234 and an
-# interim remove ran under JobID=1235 you want to package these 
-# documents by entering the command
-#     FtpInterimDocs.py 1234 1235
+# Submit the CTGovExport data to the CIPSFTP Server.
 # 
-# $Id: FtpCTGovData.py,v 1.1 2007-07-06 22:50:06 venglisc Exp $
+# $Id: FtpCTGovData.py,v 1.2 2007-08-10 16:50:15 venglisc Exp $
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2007/07/06 22:50:06  venglisc
+# Initial copy of MFP scheduling scripts.
+#
 #----------------------------------------------------------------------
-import sys, re, string, cdr, os, shutil, ftplib, time
+import sys, cdr, os, glob, ftplib, time, getopt
 
 # Setting directory and file names
 # --------------------------------
-log        = "d:\\cdr\\log\\Jobmaster.log" 
+# PUBPATH    = os.path.join('d:\\cdr', 'publishing')
+LOGFILE    = 'Jobmaster.log'
 outDir     = os.path.join('d:\\cdr', 'Output', 'NLMExport')
-pubDir     = os.path.join('d:\\cdr', 'publishing')
 dateStr    = time.strftime("%Y-%m-%d-%H%M", time.localtime())
 tarName    = "clinical_trials.tar.bz2"
 FTPSERVER  = "cipsftp.nci.nih.gov"
 FTPDIR     = "d:\\cdr\\output\\NLMExport"
 FTPUSER    = "cdrdev"
 FTPPWD     = "***REMOVED***"
+FTPLOCK    = "sending"
 
 testMode   = None
+exportDir  = None
 rmDir      = 0
 cdrDir     = 0
 divider    = "=" * 60
@@ -39,7 +35,7 @@ divider    = "=" * 60
 # Function to find the directory name that was created last
 # (Needs to be modified to allow passing the name)
 # -------------------------------------------------------------------
-def getLatestFile(directory = FTPDIR):
+def getLatestFile(baseDir = FTPDIR, dirName = None):
     """
     Function to get the last file or directory name created within
     the given directory
@@ -51,14 +47,21 @@ def getLatestFile(directory = FTPDIR):
             filename string 
     """
 
-    files = os.listdir(directory) 
+    if dirName:
+        files = [dirName]
+    else:
+        # files = os.listdir(baseDir) 
+        # Only select files of the format 'YYYYMMDDHHmmSS'
+        # ------------------------------------------------
+        os.chdir(baseDir)
+        files = glob.glob('2?????????????')
 
     # Create a list of tuples with the first element being the files
-    # timestamp.  This allows us to sort the files in chrononolic order
+    # timestamp.  This allows us to sort the files in chronologic order
     # -----------------------------------------------------------------
     allFiles = []
     for filename in files:
-        path = os.path.join(directory, filename)
+        path = os.path.join(baseDir, filename)
         allFiles.append((os.stat(path)[-2], filename))
 
     allFiles.sort()
@@ -76,12 +79,11 @@ def makeNlmFtpFile(localDirectory = 'd:/cdr/output/NLMExport',
                    compress       = 'Y'):
 
     global testMode
-    testMode = True
 
     # Setting path variables for FTP server
     # -------------------------------------
     if testMode:
-        ftpDir  = '/u/ftp/qa/nlm/test/incoming'
+        ftpDir  = '/u/ftp/test/nlm/incoming'
     else:
         ftpDir  = ftpDirectory
 
@@ -90,56 +92,140 @@ def makeNlmFtpFile(localDirectory = 'd:/cdr/output/NLMExport',
 
     # Creating the FTP command file
     # -----------------------------
-    l.write("LocalDir  = %s" % localDirectory)
-    l.write("localFile = %s" % localFile)
-    l.write("FTPDir    = %s" % ftpDir)
-    l.write("FTP Name  = %s" % ftpName)
+    l.write("  LocalDir  = %s" % localDirectory)
+    l.write("  localFile = %s" % localFile)
+    l.write("  FTPDir    = %s" % ftpDir)
+    l.write("  FTP Name  = %s" % ftpName)
 
-    # os.chdir(pubDir)
-    
-    l.write("Copy files to ftp server", stdout=True)
+    l.write("Copy files to ftp server", stdout = True)
     try:
         ftp = ftplib.FTP(FTPSERVER)
         ftp.login(FTPUSER, FTPPWD)
         chCwd = ftp.cwd(ftpDir)
-        l.write("FTP: %s" % chCwd, stdout=True)
+        l.write("FTP: %s" % chCwd, stdout = True)
+        l.write("FTPDIR: %s" % ftpDir, stdout = True)
 
+        ftp.rename(FTPLOCK, FTPLOCK + '.lck')
         l.write("Transfer file %s..." % localFile, stdout=True)
         ftp.storbinary('STOR ' + localFile, 
                             open(localDirectory + '\\' + localFile, 'rb'))
         l.write("Bytes transfered %d" % ftp.size(ftpDir + '/' + ftpName), 
                                                    stdout=True)
+        ftp.rename(FTPLOCK + '.lck', FTPLOCK)
     except ftplib.Error, msg:
         l.write('*** FTP Error ***\n%s' % msg, stdout=True)
-        sys.exit(1)
+        raise
+
+    return
+
+# ------------------------------------------------------------
+# Function to parse the command line arguments
+# ------------------------------------------------------------
+def parseArgs(args):
+
+    global testMode
+    global l
+
+    try:
+        longopts = ["testmode", "livemode",
+                    "dirname", "logfile"]
+        opts, args = getopt.getopt(args[1:], "tldo", longopts)
+    except getopt.GetoptError, e:
+        usage(args)
+
+    for o, a in opts:
+        if o in ("-o", "--logfile"):
+            global LOGFILE
+            LOGFILE = a
+            l = cdr.Log(LOGFILE)
+        elif o in ("-t", "--testmode"):
+            testMode = True
+            l.write("running in TEST mode")
+        elif o in ("-l", "--livemode"):
+            testMode = False
+            l.write("running in LIVE mode")
+        elif o in ("-d", "--dirname"):
+            exportDir = a
+            l.write("running with directory name specified")
+
+    if len(args) > 0:
+        usage(args)
+    if testMode is None:
+        usage(args)
 
     return
 
 
+# ------------------------------------------------------------
+# Function to display the default usage
+# ------------------------------------------------------------
+def usage(args):
+    print args
+    sys.stderr.write("""\
+usage: %s [--livemode|--testmode] [--nobackup]
+
+options:
+    -t, --testmode
+           run in TEST mode
+
+    -l, --livemode
+           run in LIVE mode
+
+    -d, --dirname 
+           specify the directory to be transferred    
+           (default:  Ftp directory last created)
+
+""" % sys.argv[0].split('\\')[-1])
+    sys.exit(1)
+
+
+
+# ------------------------------------------------------------
+# *** Main ***
+# Jetzt wird es ernst
+# ------------------------------------------------------------
+
 # ---------------------------------------------------------------------
 # Instantiate the Log class
 # ---------------------------------------------------------------------
-l   = cdr.Log('Jobmaster.log')
-l.write('ftpCTGovData - Started', stdout=True)
+l = cdr.Log(LOGFILE)
+l.write('FtpCTGovData - Started', stdout=True)
+l.write('Arguments: %s' % sys.argv, stdout=True)
                     
+parseArgs(sys.argv)
+
+# Setting the options for jobs which we are going to submit
+# ---------------------------------------------------------
+if testMode:
+    runmode = '--testmode'
+else:
+    runmode = '--livemode'
+
 try:
-    print "Selecting latest Directory ..."
-    latestFile = getLatestFile()
+    if exportDir:
+        print "Using specified Directory ..."
+        latestFile = getLatestFile(dirName = exportDir)
+    else:
+        print "Selecting Directory with latest timestamp..."
+        latestFile = getLatestFile()
+
     lDir       = os.path.join(outDir, latestFile[1])
 
     #print "Last File: ", latestFile
-    l.write("File Name: %s" % latestFile[1], stdout=True)
-    l.write("Mod Time:  %s" % time.strftime('%Y-%b-%d %H:%M:%S', 
-                                        time.gmtime(latestFile[0] + 500000)),
-                                        stdout=True)
+    l.write("   Base Dir:  %s" % FTPDIR, stdout = True)
+    l.write("   File Name: %s" % latestFile[1], stdout = True)
+    l.write("   Mod Time:  %s\n" % time.strftime('%Y-%b-%d %H:%M:%S', 
+                                        time.gmtime(latestFile[0])),
+                                        stdout = True)
 
     doneFtp = makeNlmFtpFile(localDirectory = lDir, compress = 'N')
 
-
 except StandardError, arg:
     l.write('*** Error - %s' % arg, stdout=True)
+    raise
 except:
-    l.write('*** Error - Unexpected failure', stdout=True)
+    l.write('*** Program exists with failure', stdout=True)
+    sys.exit(1)
 
-l.write('ftpCTGovData - Completed', stdout=True)
+l.write('FtpCTGovData - Completed', stdout=True)
 sys.exit(0)
