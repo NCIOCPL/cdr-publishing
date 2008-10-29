e4b66252872e6445204074b428690a00cedaff19
Volker Englisch <venglisch@users.noreply.github.com>
2008-10-29 22:30:01 +0000

    <R8592> Removed Kim's email address since her email account has been disabled.

--- cdr/Publishing/CheckWithdrawn.py
+++ cdr/Publishing/CheckWithdrawn.py
@@ -12,13 +12,17 @@
 # ---------------------------------------------------------------------
 # $Author: venglisc $
 # Created:          2007-12-03        Volker Englisch
-# Last Modified:    $Date: 2008-09-17 17:57:04 $
+# Last Modified:    $Date: 2008-10-29 22:30:01 $
 # 
 # $Source: /usr/local/cvsroot/cdr/Publishing/CheckWithdrawn.py,v $
-# $Revision: 1.5 $
+# $Revision: 1.6 $
 #
-# $Id: CheckWithdrawn.py,v 1.5 2008-09-17 17:57:04 venglisc Exp $
+# $Id: CheckWithdrawn.py,v 1.6 2008-10-29 22:30:01 venglisc Exp $
 # $Log: not supported by cvs2svn $
+# Revision 1.5  2008/09/17 17:57:04  venglisc
+# Added two more addresses for the distribution and removed Sheri per her
+# request.
+#
 # Revision 1.4  2008/06/03 21:43:05  bkline
 # Replaced StandardError (slated to be removed in the future) with
 # Exception objects.
@@ -272,7 +276,6 @@ SELECT dv.id, dv.num, comment, nct.value
     else:
         strTo    = ["PDQ Operator <operator@cips.nci.nih.gov>", 
                     "William Osei-Poku <william.osei-poku@lmco.com>", 
-                    "Kim Eckley <***REMOVED***>",
                     "Judy Morris <judith.morris@lmco.com>",
                     "Mark Leech <mark.j.leech@lmco.com>",
                     "James Silk <james.d.silk@lmco.com", 
