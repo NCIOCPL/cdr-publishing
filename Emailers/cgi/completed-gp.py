6005180011ce2e183988facbba4e0b9a079c7dfa
Bob Kline <bkline@rksystems.com>
2010-09-08 16:25:35 +0000

    <R9885> Final production version

--- Emailers/cgi/completed-gp.py
+++ Emailers/cgi/completed-gp.py
@@ -11,14 +11,28 @@ import util, lxml.etree as etree
 conn = util.getConnection()
 cursor = conn.cursor()
 cursor.execute("""\
-    SELECT id, bounced, completed
+    SELECT id, bounced, completed, xml <> original
       FROM gp_emailer
-     WHERE bounced IS NOT NULL
-        OR completed IS NOT NULL""")
+     WHERE recorded IS NULL
+       AND (bounced IS NOT NULL
+        OR completed IS NOT NULL)""")
 tree = etree.Element('mailers')
-for id, bounced, completed in cursor.fetchall():
-    child = etree.Element('mailer', id=id, bounced=bounced or '',
-                          completed=completed or '')
+for id, bounced, completed, modified in cursor.fetchall():
+    b = bounced and unicode(bounced) or ''
+    c = completed and unicode(completed) or ''
+    m = modified and 'Y' or 'N'
+    child = etree.Element('mailer', id=str(id), bounced=b, completed=c,
+                          modified=m)
+    tree.append(child)
+cursor.execute("""\
+    SELECT id, DATE_ADD(mailed, INTERVAL 30 DAY)
+      FROM gp_emailer
+     WHERE bounced IS NULL
+       AND completed IS NULL
+       AND recorded IS NULL
+       AND DATEDIFF(NOW(), mailed) >= 30""")
+for id, expired in cursor.fetchall():
+    child = etree.Element('mailer', id=str(id), expired=str(expired))
     tree.append(child)
 print "Content-type: text/xml\n"
 print etree.tostring(tree)
