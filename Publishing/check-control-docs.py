5bf24fa69aaafc5d872e369e5901650a0992c858
Bob Kline <bkline@rksystems.com>
2016-05-13 13:22:40 +0000

    <R14032> use latest publishable version for check

--- Publishing/check-control-docs.py
+++ Publishing/check-control-docs.py
@@ -1,5 +1,4 @@
 import cdrdb
-import glob
 import re
 import datetime
 import lxml.etree as etree
@@ -8,12 +7,20 @@ def normalize(xml):
     root = etree.XML(xml)
     xml = re.sub(r"\r+", "", etree.tostring(root, pretty_print=True))
     return xml.strip() + "\n"
+cursor = cdrdb.connect("CdrGuest").cursor()
 now = datetime.datetime.now()
 stamp = now.strftime("%Y%m%d%H%M%S")
-query = cdrdb.Query("document d", "title", "xml")
-query.join("doc_type t", "t.id = d.doc_type")
+query = cdrdb.Query("doc_version v", "v.id", "MAX(v.num)")
+query.join("doc_type t", "t.id = v.doc_type")
+query.join("active_doc a", "a.id = v.id")
 query.where("t.name = 'PublishingSystem'")
-for title, xml in query.execute().fetchall():
+query.where("v.publishable = 'Y'")
+query.group("v.id")
+for doc_id, doc_ver in query.execute(cursor).fetchall():
+    query = cdrdb.Query("doc_version", "title", "xml")
+    query.where(query.Condition("id", doc_id))
+    query.where(query.Condition("num", doc_ver))
+    title, xml = query.execute(cursor).fetchone()
     name = "%s.xml" % title
     from_fs = normalize(open(name).read())
     from_db = normalize(xml.encode("utf-8"))
